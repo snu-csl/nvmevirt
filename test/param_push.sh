6019821fc7e1fa90f87bf4f7ee543c66748b575f
Lee Seung Yun <soccer1356@ajou.ac.kr>
2023-09-08 16:39:41 +0900

    Ammend: test code add delte code trash file

--- test/param_push.sh
+++ test/param_push.sh
@@ -4,12 +4,12 @@ echo create memmap_start=35G memmap_size=4G cpus=1,1 > /sys/nvmevirt/config
 
 sleep 1
 
-echo create memmap_start=30G memmap_size=4G cpus=1,1 ftl=conv> /sys/nvmevirt/config
+echo create memmap_start=30G memmap_size=4G cpus=2,2 ftl=conv> /sys/nvmevirt/config
 
 sleep 1
 
-echo create memmap_start=25G memmap_size=4G cpus=1,1 > /sys/nvmevirt/config
+echo create memmap_start=25G memmap_size=4G cpus=3,3 > /sys/nvmevirt/config
 
 sleep 1
 
-echo create memmap_start=20G memmap_size=4G cpus=1,1 ftl=conv > /sys/nvmevirt/config
+echo create memmap_start=20G memmap_size=4G cpus=4,4 ftl=conv > /sys/nvmevirt/config
